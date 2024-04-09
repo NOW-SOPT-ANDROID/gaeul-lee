f9f3332ab4f6c72c34d9522518df145ce51ce8be
gaeulzzang <gaeulzzang@gmail.com>
2024-04-09 22:49:40 +0900

    바텀네비게이션

--- app/src/main/java/com/sopt/now/MainActivity.kt
+++ app/src/main/java/com/sopt/now/MainActivity.kt
@@ -5,11 +5,52 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.sopt.now.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val currentFragment = supportFragmentManager.findFragmentById(binding.fcvHome.id)
+        if (currentFragment == null) {
+            supportFragmentManager.beginTransaction()
+                .add(binding.fcvHome.id, HomeFragment())
+                .commit()
+        }
+
+        clickBottomNavigation()
+    }
+
+    private fun clickBottomNavigation(){
+        binding.bnvHome.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.menu_home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+
+                R.id.menu_search -> {
+                    replaceFragment(SearchFragment())
+                    true
+                }
+
+                R.id.menu_mypage -> {
+                    replaceFragment(MyPageFragment())
+                    true
+                }
+
+                else -> false
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.fcvHome.id, fragment)
+            .commit()
     }
 }
